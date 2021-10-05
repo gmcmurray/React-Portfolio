4c15ffeacc315da8e766ab540999454abd12b940
gmcmurray <83099840+gmcmurray@users.noreply.github.com>
2021-10-05 12:48:41 -0700

    added icons on footer and stackoverflow

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faStackOverflow } from '@fortawesome/free-brands-svg-icons';
 // Here we are importing a CSS file as a dependency
 import '../styles/Footer.css';
  
@@ -19,8 +20,9 @@ const styles = {
 function Footer() {
   return (
     <div className="footer" style={styles.footerContainer}>
-     <a href="https://github.com/gmcmurray/" target='_blank' rel="noreferrer" ><h4>github</h4></a>
-     <a href="https://www.linkedin.com/in/george-mcmurray/"><h4> LinkedIn </h4></a>
+     <a href="https://stackoverflow.com/c/mycellium-rising/" target='_blank' rel="noreferrer" ><FontAwesomeIcon icon={faStackOverflow} className="stackoverflow-icon"/>  <i className="fab fa-stack-overflow"></i></a>
+     <a href="https://github.com/gmcmurray/" target='_blank' rel="noreferrer" ><FontAwesomeIcon icon={faGithub} className="github-icon"/></a>
+     <a href="https://www.linkedin.com/in/george-mcmurray/"><FontAwesomeIcon icon={faLinkedin} className="linkedin-icon"/></a>
 
     </div>
   );
